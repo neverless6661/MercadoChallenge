09287ca428d96b782cdc345a00ebd46bc212b231
neverless6661 <lsca_lubani@hotmail.com>
2022-08-06 17:06:19 -0500

    Creando pantallas

---
+++ app/src/main/java/com/lubani/mercadochallenge/ListaBusqueda.java
@@ -0,0 +1,14 @@
+package com.lubani.mercadochallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaBusqueda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_busqueda);
+    }
+}
