492fa8a3fee1c8d48a8692c9dff9fbb4785cc69d
neverless6661 <lsca_lubani@hotmail.com>
2022-08-06 23:58:59 -0500

    Creando interfaces

---
+++ app/src/main/java/com/lubani/mercadochallenge/DetalleBusquedaBarra.java
@@ -0,0 +1,14 @@
+package com.lubani.mercadochallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleBusquedaBarra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_busqueda_barra);
+    }
+}
